0545bff1d70b09d5f0beac8e859c4dc4c8a6729d
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-02-08 13:53:20 +0100

    add project laravel

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/stile.css') }}">
+    <title>test prova</title>
+</head>
+<body>
+    <div class="transformed-element">
+        <h1>HELLO WORLD!</h1>
+    </div>
+</body>
+</html>
